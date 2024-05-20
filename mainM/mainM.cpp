53fb56de6749e79bd48abc3430dc96add5024da4
voltarou <bagusadnan04@gmail.com>
2024-05-20 12:00:34 +0800

    int main mainM

--- mainM/mainM.cpp
+++ mainM/mainM.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+#include "jantung.h"
+#include "manusia.h"
+
 int main()
 {
-    
+    manusia* varManusia = new manusia("jono");
+    delete varManusia;
+    return 0;
 }
 
 
