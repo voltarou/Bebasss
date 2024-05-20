73475b45d339076162fbeaf6dae1fe2eb68e43d4
voltarou <bagusadnan04@gmail.com>
2024-05-20 19:37:16 +0800

    inheritance

--- Inheritnce/Inheritnce.cpp
+++ Inheritnce/Inheritnce.cpp
@@ -2,28 +2,45 @@
 #include <string>
 using namespace std;
 
-class orang {
+class Orang {
 public:
     string nama;
 
-    orang(string pNama) :
-        nama(pNama) {
-        cout << "orang dibuat\n" << endl;
+    Orang(string pNama) : nama(pNama) {
+        cout << "Orang dibuat\n" << endl;
     }
-    ~orang() {
-        cout << "orang dihapus\n" << endl;
+
+    ~Orang() {
+        cout << "Orang dihapus\n" << endl;
     }
 
     int jumlah(int a, int b) {
         return a + b;
     }
-
 };
 
+class Pelajar : public Orang {
+public:
+    string sekolah;
 
-int main()
-{
-    
+    Pelajar(string pNama, string pSekolah) : Orang(pNama), sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+
+    ~Pelajar() {
+        cout << "Pelajar dihapus\n" << endl;
+    }
+
+    string perkenalan() {
+        return "Halo, nama saya " + nama + " dari sekolah " + sekolah + "\n";
+    }
+};
+
+int main() {
+    Pelajar siswa1("Andi Laksmono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
 }
-
-
