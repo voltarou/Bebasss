7bfd1edbc68ab2766e8a15583959f8f314b47ca6
voltarou <bagusadnan04@gmail.com>
2024-05-20 11:29:35 +0800

    ~mahasiswa

--- Zhaskkk/Zhaskkk.cpp
+++ Zhaskkk/Zhaskkk.cpp
@@ -7,6 +7,15 @@ private:
     const int id;
     string nama;
     float nilai;
+
+
+public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama) {
+
+    }
+    ~mahasiswa() {
+        cout << "Id =" << id << endl;
+    }
 };
 
 int main()
