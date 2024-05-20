dd867e11aab38d856bb6da3e4e0ddcababb12a6e
voltarou <bagusadnan04@gmail.com>
2024-05-20 18:53:50 +0800

    void pasien

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -37,7 +37,11 @@ void pasien::tambahDokter(dokter* pDokter) {
 }
 
 void pasien::cetakDokter() {
-
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
 }
 int main()
 {
