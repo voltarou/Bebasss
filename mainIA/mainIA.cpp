5b4686caf304599849d2aced3bc9a6c6bf8e0e94
voltarou <bagusadnan04@gmail.com>
2024-05-20 12:36:09 +0800

    int main

--- mainIA/mainIA.cpp
+++ mainIA/mainIA.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
 int main()
 {
-    
+    ibu* varIbu = new ibu("dini");
+    ibu* varibu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("Dewi");
 }
 
